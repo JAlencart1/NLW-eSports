4003fce406589c07507286e0766fbf509cb45462
JAlencart1 <jose.alencar.alves777@gmail.com>
2022-09-16 13:41:12 -0300

    primeiro commit

---
+++ web/src/App.tsx
@@ -0,0 +1,36 @@
+import './styles/main.css';
+
+
+function App() {
+  return (
+    <div className="w-8 h-8 bg-black">
+    
+      <h1>hello boa tarde</h1>
+   
+  </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
